721fd2ca92091c96326a3ea30801db7777b76b5a
Hadley Wickham <hadley@rstudio.com>
2015-06-12 23:50:17 +0000

    version 0.2.5

--- R/formatter.r
+++ R/formatter.r
@@ -2,13 +2,19 @@
 #'
 #' @param ... other arguments passed on to \code{\link{format}}
 #' @param x a numeric vector to format
-#' @return a function with single paramater x, a numeric vector, that
+#' @return a function with single parameter x, a numeric vector, that
 #'   returns a character vector
 #' @export
 #' @examples
 #' comma_format()(c(1, 1e3, 2000, 1e6))
 #' comma_format(digits = 9)(c(1, 1e3, 2000, 1e6))
 #' comma(c(1, 1e3, 2000, 1e6))
+#'
+#' # If you're European you can switch . and , with the more general
+#' # format_format
+#' point <- format_format(big.mark = ".", decimal.mark = ",", scientific = FALSE)
+#' point(c(1, 1e3, 2000, 1e6))
+#' point(c(1, 1.021, 1000.01))
 comma_format <- function(...) {
   function(x) comma(x, ...)
 }
@@ -26,30 +32,68 @@ comma <- function(x, ...) {
 #' any of the values has a non-zero cents and the largest value is less
 #' than \code{largest_with_cents} which by default is 100000.
 #'
-#' @return a function with single paramater x, a numeric vector, that
+#' @return a function with single parameter x, a numeric vector, that
 #'   returns a character vector
 #' @param largest_with_cents the value that all values of \code{x} must
 #'   be less than in order for the cents to be displayed
+#' @param prefix,suffix Symbols to display before and after amount.
+#' @param big.mark Character used between every 3 digits.
+#' @param negative_parens Should negative values be shown with parentheses?
+#' @param ... Other arguments passed on to \code{\link{format}}.
 #' @param x a numeric vector to format
 #' @export
 #' @examples
-#' dollar_format()(c(100, 0.23, 1.456565, 2e3))
+#' dollar_format()(c(-100, 0.23, 1.456565, 2e3))
 #' dollar_format()(c(1:10 * 10))
 #' dollar(c(100, 0.23, 1.456565, 2e3))
 #' dollar(c(1:10 * 10))
 #' dollar(10^(1:8))
-dollar_format <- function(largest_with_cents = 100000) {
+#'
+#' usd <- dollar_format(prefix = "USD ")
+#' usd(c(100, -100))
+#'
+#' euro <- dollar_format(prefix = "", suffix = "\u20ac")
+#' euro(100)
+#'
+#' finance <- dollar_format(negative_parens = TRUE)
+#' finance(c(-100, 100))
+dollar_format <- function(prefix = "$", suffix = "",
+                          largest_with_cents = 100000, ..., big.mark = ",",
+                          negative_parens = FALSE) {
   function(x) {
+    if (length(x) == 0) return(character())
     x <- round_any(x, 0.01)
-    if (max(x, na.rm = TRUE) < largest_with_cents &
-        !all(x == floor(x), na.rm = TRUE)) {
+    if (needs_cents(x, largest_with_cents)) {
       nsmall <- 2L
     } else {
       x <- round_any(x, 1)
       nsmall <- 0L
     }
-    paste0("$", format(x, nsmall = nsmall, trim = TRUE, big.mark = ",", scientific = FALSE, digits=1L))
+
+    negative <- !is.na(x) & x < 0
+    if (negative_parens) {
+      x <- abs(x)
+    }
+
+    amount <- format(abs(x), nsmall = nsmall, trim = TRUE, big.mark = big.mark,
+      scientific = FALSE, digits = 1L)
+
+    if (negative_parens) {
+      paste0(ifelse(negative, "(", ""), prefix, amount, suffix, ifelse(negative, ")", ""))
+    } else {
+      paste0(prefix, ifelse(negative, "-", ""), amount, suffix)
+    }
   }
+}
+
+needs_cents <- function(x, threshold) {
+  if (all(is.na(x)))
+    return(FALSE)
+
+  if (max(abs(x), na.rm = TRUE) > threshold)
+    return(FALSE)
+
+  !all(x == floor(x), na.rm = TRUE)
 }
 
 #' @export
@@ -58,7 +102,7 @@ dollar <- dollar_format()
 
 #' Percent formatter: multiply by one hundred and display percent sign.
 #'
-#' @return a function with single paramater x, a numeric vector, that
+#' @return a function with single parameter x, a numeric vector, that
 #'   returns a character vector
 #' @param x a numeric vector to format
 #' @export
@@ -68,6 +112,7 @@ dollar <- dollar_format()
 #' percent(runif(10, 1, 10))
 percent_format <- function() {
   function(x) {
+    if (length(x) == 0) return(character())
     x <- round_any(x, precision(x) / 100)
     paste0(comma(x * 100), "%")
   }
@@ -78,7 +123,7 @@ percent <- percent_format()
 
 #' Scientific formatter.
 #'
-#' @return a function with single paramater x, a numeric vector, that
+#' @return a function with single parameter x, a numeric vector, that
 #'   returns a character vector
 #' @param digits number of significant digits to show
 #' @param ... other arguments passed on to \code{\link{format}}
@@ -102,10 +147,41 @@ scientific <- function(x, digits = 3, ...) {
   format(x, trim = TRUE, scientific = TRUE, ...)
 }
 
+#' Ordinal formatter: add ordinal suffixes (-st, -nd, -rd, -th) to numbers.
+#'
+#' @return a function with single paramater x, a numeric vector, that
+#'   returns a character vector
+#' @param x a numeric vector to format
+#' @export
+#' @examples
+#' ordinal_format()(1:10)
+#' ordinal(1:10)
+
+ordinal_format <- function(x) {
+  function(x) ordinal(x)
+}
+
+#' @export
+#' @rdname ordinal_format
+ordinal <- function(x) {
+  stopifnot(all(x > 0))
+
+  suffixes <- list(
+    st = "(?<!1)1$",
+    nd = "(?<!1)2$",
+    rd = "(?<!1)3$",
+    th = "(?<=1)[123]$",
+    th = "[0456789]$"
+  )
+
+  out <- stack(llply(suffixes, grep, x = x, perl = TRUE))
+  paste0(comma(x), out$ind[order(out$values)])
+}
+
 #' Parse a text label to produce expressions for plotmath.
 #'
 #' @seealso \code{\link{plotmath}}
-#' @return a function with single paramater x, a character vector, that
+#' @return a function with single parameter x, a character vector, that
 #'    returns a list of expressions
 #' @export
 #' @examples
@@ -123,7 +199,7 @@ parse_format <- function() {
 #' @param format another format function to apply prior to mathematical
 #'   transformation - this makes it easier to use floating point numbers in
 #'   mathematical expressions.
-#' @return a function with single paramater x, a numeric vector, that
+#' @return a function with single parameter x, a numeric vector, that
 #'    returns a list of expressions
 #' @export
 #' @seealso \code{\link{plotmath}}
@@ -145,11 +221,26 @@ math_format <- function(expr = 10 ^ .x, format = force) {
 }
 globalVariables(".x")
 
+#' Wrap text to a specified width, adding newlines for spaces if text exceeds
+#' the width
+#'
+#' @param width value above which to wrap
+#' @return Function with single parameter x, a character vector, that
+#'    returns a wrapped character vector
+#' @export
+#' @examples
+#' wrap_10 <- wrap_format(10)
+#' wrap_10('A long line that needs to be wrapped')
+wrap_format <- function(width) {
+  function(x) {
+    unlist(lapply(strwrap(x, width = width, simplify = FALSE), paste0, collapse = "\n"))
+  }
+}
 #' Format labels after transformation.
 #'
 #' @param trans transformation to apply
 #' @param format additional formatter to apply after transformation
-#' @return a function with single paramater x, a numeric vector, that
+#' @return a function with single parameter x, a numeric vector, that
 #'    returns a character vector of list of expressions
 #' @export
 #' @examples
@@ -183,6 +274,28 @@ format_format <- function(...) {
 precision <- function(x) {
   rng <- range(x, na.rm = TRUE)
 
-  span <- if (zero_range(rng)) rng[1] else diff(rng)
+  span <- if (zero_range(rng)) abs(rng[1]) else diff(rng)
   10 ^ floor(log10(span))
+}
+
+#' Add units to the labels
+#'
+#' @param unit The units to append
+#' @param scale A scaling factor. Useful if the underlying data is on another scale
+#' @param sep The separator between the number and the label
+#' @param ... Arguments passed on to \code{\link{format}}
+#' @export
+#' @examples
+#' # labels in kilometer when the raw data are in meter
+#' km <- unit_format(unit = "km", scale = 1e-3, digits = 2)
+#' km(runif(10) * 1e3)
+#'
+#' # labels in hectares, raw data in square meters
+#' ha <- unit_format(unit = "ha", scale = 1e-4)
+#' km(runif(10) * 1e5)
+#' @seealso \code{\link{comma}}
+unit_format <- function(unit = "m", scale = 1, sep = " ", ...){
+  function(x){
+    paste(comma(x * scale, ...), unit, sep = sep)
+  }
 }
--- tests/testthat/test-formatter.r
+++ tests/testthat/test-formatter.r
@@ -16,43 +16,29 @@ test_that("scientific format shows specific sig figs", {
   expect_equal(scientific(0.123456, digits = 3), "1.23e-01")
 })
 
-test_that("dollar format", {
-  expect_equal(dollar(c(100, 0.23, 1.456565, 2e3)),
-               c("$100.00", "$0.23", "$1.46", "$2,000.00"))
-  expect_equal(dollar(c(1:10 * 10)),
-               c("$10", "$20", "$30", "$40", "$50", "$60", "$70",
-                 "$80", "$90", "$100"))
-  expect_equal(dollar((1:10 * 10) + 0.01),
-               c("$10.01", "$20.01", "$30.01", "$40.01", "$50.01", "$60.01",
-                 "$70.01", "$80.01", "$90.01", "$100.01"))
-  expect_equal(dollar((1:10 * 10) + 0.001),
-               c("$10", "$20", "$30", "$40", "$50", "$60", "$70",
-                 "$80", "$90", "$100"))
-  expect_equal(dollar(10^(1:8)),
-               c("$10", "$100", "$1,000", "$10,000", "$100,000",
-                 "$1,000,000", "$10,000,000", "$100,000,000"))
-  expect_equal(dollar(seq(10111.11, 100000, 10111.11)),
-               c("$10,111.11", "$20,222.22", "$30,333.33", "$40,444.44",
-                 "$50,555.55", "$60,666.66", "$70,777.77", "$80,888.88",
-                 "$90,999.99"))
-  expect_equal(dollar(seq(101111.11, 1000000, 101111.11)),
-               c("$101,111", "$202,222", "$303,333", "$404,444", "$505,556",
-                 "$606,667", "$707,778", "$808,889", "$910,000"))
-  expect_equal(dollar(seq(101111, 1000000, 101111)),
-               c("$101,111", "$202,222", "$303,333", "$404,444", "$505,555",
-                 "$606,666", "$707,777", "$808,888", "$909,999"))
-  expect_equal(dollar(seq(10111111.11, 100000000, 10111111.11)),
-               c("$10,111,111", "$20,222,222", "$30,333,333", "$40,444,444",
-                 "$50,555,556", "$60,666,667", "$70,777,778", "$80,888,889",
-                 "$91,000,000"))
-  expect_equal(dollar((1:10) * 100000),
-               c("$100,000", "$200,000", "$300,000", "$400,000", "$500,000",
-                 "$600,000", "$700,000", "$800,000", "$900,000", "$1,000,000"))
-  expect_equal(dollar(((1:10) * 100000) + 0.01),
-               c("$100,000", "$200,000", "$300,000", "$400,000", "$500,000",
-                 "$600,000", "$700,000", "$800,000", "$900,000", "$1,000,000"))
-  expect_equal(dollar(c(9.999)), c("$10"))
-  expect_equal(dollar(c(99.999)), c("$100"))
+test_that("wrap correctly wraps long lines", {
+  expect_equal(wrap_format(10)("this is a long line"),
+               "this is a\nlong line")
+  expect_equal(wrap_format(10)(c("this is a long line", "this is another long line")),
+               c("this is a\nlong line", "this is\nanother\nlong line"))
+  expect_equal(wrap_format(10)("a_very_long_word_without_spaces"),
+               "a_very_long_word_without_spaces")
+  expect_equal(wrap_format(10)("short line"), "short\nline")
+  expect_equal(wrap_format(15)("short line"), "short line")
+})
+
+test_that("ordinal format", {
+  expect_equal(ordinal(1),  "1st")
+  expect_equal(ordinal(2),  "2nd")
+  expect_equal(ordinal(3),  "3rd")
+  expect_equal(ordinal(4),  "4th")
+  expect_equal(ordinal(11), "11th")
+  expect_equal(ordinal(12), "12th")
+  expect_equal(ordinal(21), "21st")
+})
+
+test_that("ordinal format maintains order", {
+  expect_equal(ordinal(c(21, 1, 11)), c("21st", "1st", "11th"))
 })
 
 test_that("formatters don't add extra spaces", {
@@ -63,5 +49,54 @@ test_that("formatters don't add extra spaces", {
   expect_false(has_space(dollar(x)))
   expect_false(has_space(percent(x)))
   expect_false(has_space(percent(x)))
-  expect_false(has_space(scientific(x)))  
+  expect_false(has_space(scientific(x)))
+})
+
+test_that("formats work with 0 length input", {
+  x <- numeric()
+  expected <- character()
+  expect_identical(comma(x), expected)
+  expect_identical(dollar(x), expected)
+  expect_identical(percent(x), expected)
+  expect_identical(scientific(x), expected)
+  expect_identical(comma_format()(x), expected)
+  expect_identical(date_format()(as.Date(character(0))), expected)
+  expect_identical(dollar_format()(x), expected)
+  expect_identical(math_format()(x), list())
+  expect_identical(parse_format()(x), list())
+  expect_identical(percent_format()(x), expected)
+  expect_identical(scientific_format()(x), expected)
+  expect_identical(trans_format(identity)(x), expected)
+})
+
+test_that("unit format", {
+  expect_equal(
+    unit_format(unit = "km", scale = 1e-3)(c(1e3, 2e3)),
+    c("1 km", "2 km")
+  )
+  expect_equal(
+    unit_format(unit = "ha", scale = 1e-4)(c(1e3, 2e3)),
+    c("0.1 ha", "0.2 ha")
+  )
+  expect_equal(
+    unit_format()(c(1e3, 2e3)),
+    c("1,000 m", "2,000 m")
+  )
+})
+
+
+# Percent formatter -------------------------------------------------------
+
+test_that("negative percents work", {
+  expect_equal(percent(-0.6), "-60%")
+})
+
+# Dollar formatter --------------------------------------------------------
+
+test_that("negative comes before prefix", {
+  expect_equal(dollar(-1), "$-1")
+})
+
+test_that("missing values preserved", {
+  expect_equal(dollar(NA_real_), "$NA")
 })
